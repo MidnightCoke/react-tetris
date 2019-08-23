fc0d52984acdd787c556759aabd5b669393cd7c6
efe <jimmyfallon717@gmail.com>
2019-08-23 19:39:51 +0300

    shadows are working well

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,9 +3,7 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={type} color={TETROMINOS[type].color}>
-        Cell
-    </StyledCell>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color} />
 )
 
 export default Cell;
